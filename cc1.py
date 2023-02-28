eeba2b6e042238dfd3d55d883e45b4a7dfa87638
Karl Feaster <feaster11c@gmail.com>
2023-02-27 21:00:20 -0500

    added context to cc1 from nucamp assignment

--- cc1.py
+++ cc1.py
@@ -1,10 +1,18 @@
-Turn this line into a comment
+#Turn this line into a comment
 
-cities = [Seattle, "Tacoma', "Bellevue"]
+cities = ['Seattle', "Tacoma", "Bellevue"] #reminder that this is a list
 
 a = 1
 print(type(a))
 
+b = 4.14
+print(type(b))
+
+c = 'Hiii Babe'
+print(type(c))
+
+d = a + b
+print(d)
 
 
 
