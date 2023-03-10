7a657cd48c7a9b4b848e03e671f82c19f4ff7e08
Karl Feaster <feaster11c@gmail.com>
2023-03-09 20:07:46 -0500

    added function examples to be used from nucamp

---
+++ builtin_functions.py
@@ -0,0 +1,15 @@
+print('Math Functions')
+
+abs_int = abs(-1)
+print(abs_int)
+
+int_to_float = float(100)
+print(int_to_float)
+
+#note that you can use the print func directly on another function if you do not need to save the return
+
+print(int(1.23))
+print(max(1, 2, -5, 10, 0))
+print(min(1, 2, -5, 10, 0))
+print(pow(2,3))
+print(round(51.6))
