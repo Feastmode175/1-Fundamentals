ecde1975becb6f1f13aad7ddbbf1682113a527e8
Karl Feaster <feaster11c@gmail.com>
2023-03-10 11:25:33 -0500

    added code to show alternate import method

---
+++ use_module2.py
@@ -0,0 +1,6 @@
+from my_module import greet, flavor
+#when using the from keyword, you can specify what to import
+#this eliminates the need to use dot notation on imported code
+
+greet('Albert Einstein')
+print('My favorite ice cream flavor is', flavor)
