b6ae4b489a00f87896ab4a6bb317caeff520fdf3
Karl Feaster <feaster11c@gmail.com>
2023-03-10 11:21:51 -0500

    added dot notation to imported content

--- use_module.py
+++ use_module.py
@@ -1,4 +1,4 @@
-import my_module
+import my_module #note that any content from the module must be prepended by module name
 
-greet('Alber Einstein')
-print('My favorite ice cream flavor is', flavor)
+my_module.greet('Alber Einstein')
+print('My favorite ice cream flavor is', my_module.flavor)
