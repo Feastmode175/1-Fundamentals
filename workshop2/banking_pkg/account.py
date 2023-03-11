210bba4a8160898b7587b9d52e694620ea5fc285
Karl Feaster <feaster11c@gmail.com>
2023-03-11 13:08:13 -0500

    added print statements to functions for clarity

--- workshop2/banking_pkg/account.py
+++ workshop2/banking_pkg/account.py
@@ -3,10 +3,12 @@ def show_balance(balance):
   
 def deposit(balance):
   deposit_amount = input('Enter amount to deposit: ')
+  print('You have deposited:', deposit_amount)
   return balance + float(deposit_amount)
 
 def withdraw(balance):
   withdrawal_amount = input('Enter amount to withdraw: ')
+  print('You have withdrawn:', withdrawal_amount)
   return balance - float(withdrawal_amount)
 
 def logout(name):
