7df1bd11d7f1eb078f109f2230ac4b6ba6586ebf
Karl Feaster <feaster11c@gmail.com>
2023-03-11 00:08:04 -0500

    added beginning input statement

--- cc_dicegame.py
+++ cc_dicegame.py
@@ -4,6 +4,9 @@ high_score = 0
 
 
 def dice_game():
-
-
+  print('Current High Score: ', high_score)
+  print('1) Roll Dice \n2) Leave Game')
+  roll_dice = input('Enter your choice: ')
+  
+  
 dice_game()
