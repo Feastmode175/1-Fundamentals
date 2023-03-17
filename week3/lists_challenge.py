9269555441ab3c6ff0d96e415bd3fd879dead768
Karl Feaster <feaster11c@gmail.com>
2023-03-17 19:56:49 -0400

    added random import and started loop

--- week3/lists_challenge.py
+++ week3/lists_challenge.py
@@ -1,1 +1,6 @@
-lists = ["AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD"]
+import random
+
+diamonds = ["AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD"]
+hand = []
+
+while diamonds:
