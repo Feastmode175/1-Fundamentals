d59a69a4c4994ee9cc39511b0f22905fcf5f3df1
Karl Feaster <feaster11c@gmail.com>
2023-03-18 12:02:40 -0400

    boiler plate for options list

--- week3/workshop-3/app.py
+++ week3/workshop-3/app.py
@@ -6,12 +6,32 @@ database = {
 
 donations = []
 
-authorized_user =''
+authorized_user = ''
 
 show_homepage()
 
+# ensure user is logged in and display user name
 if authorized_user == '':
   print('You must be logged in to donate.')
 else:
   print('You are logged in as: ', authorized_user)
 
+option = input('Please choose an option.')
+
+#define user options
+
+if option == '1':
+  print('Test 1')
+  #TODO write login functionality
+elif option == '2':
+  print('Test 2')
+  #TODO: write register functionality
+elif option == '3':
+  print('Test 3')
+  #TODO: write donate functionality
+elif option == '4':
+  print('Test 4')
+  #TODO: write show donations functionality
+elif option == '5':
+  print('Test 5')
+  print('Goodbye', authorized_user)
