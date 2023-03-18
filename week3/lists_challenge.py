f12b627d50c384bfabbc47487ec4ca0178aade7f
Karl Feaster <feaster11c@gmail.com>
2023-03-17 20:41:10 -0400

    added logic to select random cards and add to hand

--- week3/lists_challenge.py
+++ week3/lists_challenge.py
@@ -4,3 +4,17 @@ diamonds = ["AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "
 hand = []
 
 while diamonds:
+  draw_card = input('Press Enter to pick a card. \nPress "Q" followed by enter to quit.\n')
+  if draw_card.lower() == 'q':
+    print('\nYou have quit. \nGoodbye!\n\n')
+    break
+  else:
+    random_card = random.choice(diamonds)
+    diamonds.remove(random_card)
+    hand.append(random_card)
+    
+    print('Your hand:', hand)
+    print('Remaining cards:', diamonds)
+    
+if not diamonds:
+  print('There are no more cards to pick.')
