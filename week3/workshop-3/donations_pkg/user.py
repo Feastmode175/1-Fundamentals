593fb8bcae31bd98f6cc7cc9b4780d7123dfb3ba
Karl Feaster <feaster11c@gmail.com>
2023-03-18 12:15:40 -0400

    added newlines

--- week3/workshop-3/donations_pkg/user.py
+++ week3/workshop-3/donations_pkg/user.py
@@ -3,8 +3,8 @@ def login(database, username, password):
     print ('Welcome', username + '!')
     return username
   elif username in database and database[username] != password:
-    print('Incorrect password')
+    print('Incorrect password \n')
     return ''
   else:
-    print('Username not found')
+    print('Username not found \n')
     return ''
