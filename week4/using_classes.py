c313d2a4fe3b3699806148b2af74dd4b90489adf
Karl Feaster <feaster11c@gmail.com>
2023-03-22 14:56:56 -0400

    changed Player max hp

--- week4/using_classes.py
+++ week4/using_classes.py
@@ -6,4 +6,9 @@ class Player:
 player1 = Player()
 print(player1.max_hp) #access attribute of player class
 player2 = Player()
-print(player2.max_hp) 
+print(player2.max_hp)
+
+#value of class attribute changed (affects all associated objects)
+Player.max_hp = 5000
+print(player1.max_hp)
+print(player2.max_hp)
