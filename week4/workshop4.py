fe49f5f9a137d7cf12b548bfefddde9c0be492f6
Karl Feaster <feaster11c@gmail.com>
2023-03-28 11:17:37 -0400

    added bank user methods

--- week4/workshop4.py
+++ week4/workshop4.py
@@ -23,6 +23,20 @@ class BankUser(User):
     super().__init__(name, pin, password)
     self.balance = 0
 
+  #show current bank user balance
+  def show_balance(self):
+    print(self.name, 'has an account balance of:', self.balance)
+    
+  # withdraw specified amount
+  # TODO: if withdrawal amount is negative, produce error
+  def withdraw(self, withdrawal_amount):
+    self.balance -= withdrawal_amount
+    return self.balance
+  
+  #deposit specified amount
+  def deposit(self, deposit_amount):
+    self.balance += deposit_amount
+    return self.balance
 
 
 
@@ -32,8 +46,8 @@ class BankUser(User):
 
 """ Driver Code for Task 1 """
 #instantiate test_user and show attributes
-test_user = User('Bob', 1234, 'password')
-""" print(test_user.name)
+""" test_user = User('Bob', 1234, 'password')
+print(test_user.name)
 print(test_user.pin)
 print(test_user.password) """
 
@@ -49,7 +63,7 @@ print(test_user.password) """
 
 """ Driver Code for Task 3 """
 # instantiate test bank user and test functionality
-test_bank_user = BankUser('Chuck', 5678, 'password123')
+""" test_bank_user = BankUser('Chuck', 5678, 'password123')
 print(test_bank_user.name)
 print(test_bank_user.pin)
 print(test_bank_user.password)
@@ -62,4 +76,13 @@ test_user.change_password('password456')
 print(test_bank_user.name)
 print(test_bank_user.pin)
 print(test_bank_user.password)
-print(test_bank_user.balance)
+print(test_bank_user.balance) """
+
+""" Driver Code for Task 4"""
+#test bank user methods
+""" test_bank_user = BankUser('Chuck', 5678, 'password123')
+test_bank_user.show_balance()
+test_bank_user.deposit(1000.00)
+test_bank_user.show_balance()
+test_bank_user.withdraw(500.00)
+test_bank_user.show_balance() """
