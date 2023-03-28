65818bb21b535c66a913887a41563d6d3711191f
Karl Feaster <feaster11c@gmail.com>
2023-03-28 08:38:32 -0400

    added stack variable with tests

--- week4/stack2.py
+++ week4/stack2.py
@@ -5,11 +5,26 @@ class Stack:
   def push(self, value):
     self.items.append(value)
     
-def size(self):
-  return len(self.items)
+  def pop(self):
+    if self.size() == 0:
+      return None
+    return self.items.pop()
 
-def pop(self):
-  if self.size() == 0:
-    return None
-  return self.items.pop()
-  
+  def size(self):
+    return len(self.items)
+
+
+
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+
+print("Pass" if (stack.size() == 5) else "Fail")
+stack.push(5)
+print("Pass" if (stack.size() == 5) else "Fail")
+
+# remember that pop returns the popped value
+print("Pass" if (stack.pop() == 5) else "Fail")
+print("Pass" if (stack.pop() == 4) else "Fail")
