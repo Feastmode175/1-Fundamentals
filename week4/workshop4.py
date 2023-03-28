fcb407cf88fce9036773744f86b3f3576a32db01
Karl Feaster <feaster11c@gmail.com>
2023-03-28 10:59:56 -0400

    added bank user subclass with balance

--- week4/workshop4.py
+++ week4/workshop4.py
@@ -18,6 +18,11 @@ class User:
     self.password = new_password
 
 
+class BankUser(User):
+  def __init__(self, name, pin, password,):
+    super().__init__(name, pin, password)
+    self.balance = 0
+
 
 
 
@@ -28,16 +33,33 @@ class User:
 """ Driver Code for Task 1 """
 #instantiate test_user and show attributes
 test_user = User('Bob', 1234, 'password')
-print(test_user.name)
+""" print(test_user.name)
 print(test_user.pin)
-print(test_user.password)
+print(test_user.password) """
 
 """ Driver Code for Task 2 """
 #change test user attributes and display
-test_user.change_name('Bobby')
+""" test_user.change_name('Bobby')
 test_user.change_pin('4321')
-test_user.change_password('newpassword')
-
+test_user.change_password('newpassword') """
+""" 
 print(test_user.name)
 print(test_user.pin)
-print(test_user.password)
+print(test_user.password) """
+
+""" Driver Code for Task 3 """
+# instantiate test bank user and test functionality
+test_bank_user = BankUser('Chuck', 5678, 'password123')
+print(test_bank_user.name)
+print(test_bank_user.pin)
+print(test_bank_user.password)
+print(test_bank_user.balance)
+
+test_bank_user.change_name('Chucky')
+test_user.change_pin('8765')
+test_user.change_password('password456')
+
+print(test_bank_user.name)
+print(test_bank_user.pin)
+print(test_bank_user.password)
+print(test_bank_user.balance)
