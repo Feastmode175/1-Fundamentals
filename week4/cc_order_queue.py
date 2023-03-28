e0670bd3a26720f8db787116a7aeaf98763140b4
Karl Feaster <feaster11c@gmail.com>
2023-03-28 09:29:08 -0400

    added ice cream shop methods and test cases

--- week4/cc_order_queue.py
+++ week4/cc_order_queue.py
@@ -22,6 +22,7 @@ class IceCreamShop:
     self.flavors = flavors
     self.orders = Queue()
     
+  
   def take_order(self, customer, flavor, scoops):
     if flavor not in self.flavors:
       print('Sorry! We do not have that flavor.')
@@ -31,9 +32,27 @@ class IceCreamShop:
       
     else:
       order = {
-        'customer': customer,
-        'flavor': flavor,
-        'scoops': scoops
+        'Customer': customer,
+        'Flavor': flavor,
+        'Scoops': scoops
         }
       self.orders.enqueue(order)
       print('Order created!')
+      
+  def show_all_orders(self):
+    print('\nAll Pending Ice Cream Orders:')
+    for order in self.orders.items:
+      print(order)
+      
+  def next_order(self):
+    self.orders.dequeue
+    
+
+shop = IceCreamShop(["rocky road", "mint chip", "pistachio"])
+shop.take_order("Zachary", "pistachio", 3)
+shop.take_order("Marcy", "mint chip", 1)
+shop.take_order("Leopold", "vanilla", 2)
+shop.take_order("Bruce", "rocky road", 0)
+shop.show_all_orders()
+shop.next_order()
+shop.show_all_orders()
