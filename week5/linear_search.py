510c30b657cdce3e86cd64e754fe0e4402036a6f
Karl Feaster <feaster11c@gmail.com>
2023-03-31 16:29:29 -0400

    added baseline linear search function

---
+++ week5/linear_search.py
@@ -0,0 +1,5 @@
+def linear_search(the_list, target):
+  for x in range(len(the_list)):   #note that providing only one value for range uses the value as the stop value
+    if the_list[x] == target:
+      print('Found at index', x)
+      return x
