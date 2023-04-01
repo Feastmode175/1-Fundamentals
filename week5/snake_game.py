0a0eeb8f7487a470cd3cb9460b232dd75c2e2ecf
Karl Feaster <feaster11c@gmail.com>
2023-04-01 08:28:37 -0400

    removed unused color option

--- week5/snake_game.py
+++ week5/snake_game.py
@@ -15,7 +15,6 @@ colors = {
   'white': pygame.Color(255, 255, 255), 
   'red': pygame.Color(255, 0, 0),
   'green':pygame.Color(0, 255, 0),
-  'blue': pygame.Color(0, 0, 255)
   }
 
 #set the game display settings
